3989a44a6150f71932607bdbddfec36ad6546cd9
YCPyc <yp9ar@virginia.edu>
2024-07-31 17:59:55 -0400

    connected to google sheets for district and school info

--- api/routes/demo.js
+++ api/routes/demo.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("./firebase");
 var fetch = require("node-fetch");
+const fs = require("fs").promises;
+const path = require("path");
+const process = require("process");
+const { authenticate } = require("@google-cloud/local-auth");
+const { google } = require("googleapis");
 
 const { getDocs, collection } = require("firebase/firestore");
+const { JWT } = require("google-auth-library");
+const serviceCredentials = require("../serviceCredentials.json");
+const { version } = require("os");
 
 //firebase-not currently using
 router.get("/info", async (req, res, next) => {
@@ -34,24 +42,26 @@ router.post("/getMonday", async (req, res, next) => {
   // .then(res => console.log(JSON.stringify(res, null, 2)));
 });
 
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 //get data from google shet
 router.post("/getDistrictSchool", async (req, res, next) => {
-  fetch("https://api.monday.com/v2", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjIzNDI2ODE2OCwidWlkIjozMTI4ODQ0NCwiaWFkIjoiMjAyMy0wMi0wM1QwMDozNjoyMC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6ODg4NDgxOSwicmduIjoidXNlMSJ9.oM37gRdrLf8UnnmuZIM-QWDRoT_GtgFLLyHpvnxGUtQ",
-      "API-Version": "2024-04",
-    },
-    body: JSON.stringify({
-      query: req.body.query,
-      // 'variables' : JSON.stringify(req.body.vars)
-    }),
-  })
-    .then((res) => res.json())
-    .then((result) => res.status(200).send(result));
-  // .then(res => console.log(JSON.stringify(res, null, 2)));
+  const auth = new JWT({
+    email: serviceCredentials.client_email,
+    key: serviceCredentials.private_key,
+    scopes: SCOPES,
+  });
+  const googleSheets = google.sheets({ version: "v4", auth });
+  const data = await googleSheets.spreadsheets.values.get({
+    spreadsheetId: "11jlo9UeWxZGwunhDb24hZBwAKc5b8ZKM9AYNWZaUyZY",
+    range: "FY25 School/District Selection for Sites!A:Z",
+    majorDimension: "COLUMNS",
+  });
+  const rows = data.data.values;
+  if (!rows || rows.length === 0) {
+    console.log("No data found.");
+    return res.status(500);
+  }
+  return res.status(200).send(rows);
 });
 
 //upload data to Monday
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,8 +41,8 @@ function App() {
             <NavBar userStatus={isUserSignedIn} />
             <Routes>
               {/* <Route element={<Home/>} path="/"/> */}
-              <Route element={<FormPage />} path="/" />
-              <Route element={<CoachLogPage />} path="/coachlog" />
+              <Route element={<FormPage />} path="/weekly-project-log" />
+              <Route element={<CoachLogPage />} path="/" />
             </Routes>
           </AccessTokenProvider>
         </BrowserRouter>
