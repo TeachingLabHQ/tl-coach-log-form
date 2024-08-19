dad6793122721d45d4bdf342a3e1893fe6f32bc1
YCPyc <yp9ar@virginia.edu>
2024-08-19 17:27:11 -0400

    remove manual name option

--- frontend/src/components/utils/EmployeeNameQuestion.js
+++ frontend/src/components/utils/EmployeeNameQuestion.js
@@ -59,30 +59,11 @@ export const EmployeeNameQuestion = () => {
               {val.name}
             </option>
           ))}
-          <option value={JSON.stringify({ name: "missing name", id: 0 })}>
-            Others: My name is not here
-          </option>
         </Form.Control>
         <Form.Control.Feedback type="invalid">
           Please choose a name.
         </Form.Control.Feedback>
       </Form.Group>
-
-      {selectedCoach === "missing name" ? (
-        <Form.Group className="mb-5" controlId="formBasicSite">
-          <Form.Label>
-            <strong>Please enter your full name:*</strong>
-          </Form.Label>
-          <Form.Control
-            name="coachNameManual"
-            as="input"
-            required
-          ></Form.Control>
-          <Form.Control.Feedback type="invalid">
-            Please input a name.
-          </Form.Control.Feedback>
-        </Form.Group>
-      ) : null}
     </>
   );
 };
