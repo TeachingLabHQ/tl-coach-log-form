b592f93361cb1880444ea8518b516cdf617026d5
YCPyc <yp9ar@virginia.edu>
2025-04-03 15:59:38 -0400

    feat:temp hide the convo log form

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -62,9 +62,9 @@ function NavBar(props) {
               <Nav.Link as={Link} to="/">
                 Coach Log
               </Nav.Link>
-              <Nav.Link as={Link} to="/conversation-record">
+              {/* <Nav.Link as={Link} to="/conversation-record">
                 Conversation Log
-              </Nav.Link>
+              </Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
